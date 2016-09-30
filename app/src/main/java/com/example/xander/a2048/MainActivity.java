09e853eb076d828b40df944ad253c555a4b11e76
sateeprikker <boo.mars@hotmail.com>
2016-09-30 20:30:28 +0200

    update

--- app/src/main/java/com/example/xander/a2048/MainActivity.java
+++ app/src/main/java/com/example/xander/a2048/MainActivity.java
@@ -11,5 +11,10 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
 
 }
